cc9eb1f6ef192aa1f40bd12a755bd666fcce4e6d
kashifest <kashif.khan@est.tech>
2021-12-15 18:37:57 +0200

    Downgrade go to 1.16

--- hack/unit.sh
+++ hack/unit.sh
@@ -17,6 +17,6 @@ else
     --volume "${PWD}:/metal3-ipam:ro,z" \
     --entrypoint sh \
     --workdir /metal3-ipam \
-    docker.io/golang:1.17 \
+    docker.io/golang:1.16 \
     /metal3-ipam/hack/unit.sh "${@}"
 fi;
