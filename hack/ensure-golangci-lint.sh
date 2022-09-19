6ab13b4d6bb38be4465615f3bf0a274f2edab3f7
Furkat Gofurov <furkat.gofurov@est.tech>
2022-09-19 15:42:46 +0300

    Add missing golangci-lint.sh script under hack folder

---
+++ hack/ensure-golangci-lint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eux
+
+IS_CONTAINER=${IS_CONTAINER:-false}
+CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-podman}"
+
+if [ "${IS_CONTAINER}" != "false" ]; then
+  export XDG_CACHE_HOME=/tmp/.cache
+  mkdir /tmp/unit
+  cp -r . /tmp/unit
+  cd /tmp/unit
+  make lint
+else
+  "${CONTAINER_RUNTIME}" run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/metal3-ipam:ro,z" \
+    --entrypoint sh \
+    --workdir /metal3-ipam \
+    docker.io/golang:1.17 \
+    /metal3-ipam/hack/ensure-golangci-lint.sh
+fi;
