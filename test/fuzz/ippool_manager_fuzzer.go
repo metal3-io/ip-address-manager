b7be3f49ef9a15e1dd72f83f3b582feb8783b79e
Jawad Zaheer <jawad.zaheer@est.tech>
2023-04-27 14:36:18 +0000

    Added fuzzing targets to ipam.

---
+++ test/fuzz/ippool_manager_fuzzer.go
@@ -0,0 +1,43 @@
+package ipam
+
+import (
+	"fmt"
+
+	fuzz "github.com/AdaLogics/go-fuzz-headers"
+	"github.com/go-logr/logr"
+	ipamv1 "github.com/metal3-io/ip-address-manager/api/v1alpha1"
+	ipam "github.com/metal3-io/ip-address-manager/ipam"
+)
+
+func FuzzNewIPPoolManager(data []byte) int {
+	f := fuzz.NewConsumer(data)
+	ipPool := &ipamv1.IPPool{}
+
+	err := f.GenerateStruct(ipPool)
+	if err != nil {
+		return 0
+	}
+
+	fmt.Printf("before %+v\n", ipPool)
+
+	ipPoolMgr, err := ipam.NewIPPoolManager(nil, ipPool,
+		logr.Discard(),
+	)
+	if err != nil {
+		return 0
+	}
+
+	ipPoolMgr.SetFinalizer()
+	fmt.Printf("after %+v\n", ipPool)
+
+	if len(ipPool.ObjectMeta.Finalizers) == 0 {
+		return 0
+	}
+
+	ipPoolMgr.UnsetFinalizer()
+
+	if len(ipPool.ObjectMeta.Finalizers) != 0 {
+		return 0
+	}
+	return 1
+}
