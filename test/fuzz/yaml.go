667cd67f63346e016d3b617c1c22b4923c927121
Jawad Zaheer <jawad.zaheer@est.tech>
2023-04-28 06:23:34 +0000

    Added IpClaim pooling test.

--- test/fuzz/yaml.go
+++ test/fuzz/yaml.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/go-logr/logr"
 	ipamv1 "github.com/metal3-io/ip-address-manager/api/v1alpha1"
+	controller "github.com/metal3-io/ip-address-manager/controllers"
 	ipam "github.com/metal3-io/ip-address-manager/ipam"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	sigyaml "sigs.k8s.io/yaml"
 )
 
@@ -38,6 +40,48 @@ func IPPoolFinalizer(b []byte) int {
 
 	if len(ipamv1.IPPoolFinalizer) != 0 {
 		return 0
+	}
+	return 1
+}
+
+type TestCaseM3IPCToM3IPP struct {
+	IPClaim       *ipamv1.IPClaim
+	ExpectRequest bool
+}
+
+func IPClaimToPool(b []byte) int {
+	var tc TestCaseM3IPCToM3IPP
+	if err := sigyaml.Unmarshal(b, &tc); err != nil {
+		return 0
+	}
+	r := controller.IPPoolReconciler{}
+	obj := client.Object(tc.IPClaim)
+	reqs := r.IPClaimToIPPool(obj)
+
+	if tc.ExpectRequest {
+		if len(reqs) != 1 {
+			return 0
+		}
+
+		req := reqs[0]
+		if req.NamespacedName.Name != tc.IPClaim.Spec.Pool.Name {
+			return 0
+		}
+
+		if tc.IPClaim.Spec.Pool.Namespace == "" && req.NamespacedName.Namespace != tc.IPClaim.Namespace {
+			return 0
+
+		} else {
+			if req.NamespacedName.Namespace != tc.IPClaim.Spec.Pool.Namespace {
+				return 0
+			}
+		}
+
+	} else {
+		if len(reqs) != 0 {
+			return 0
+		}
+
 	}
 	return 1
 }
