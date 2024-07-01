8e13c64f216f07ff538baaa23849c025465d2094
Tuomo Tanskanen <tuomo.tanskanen@est.tech>
2024-07-01 08:17:12 +0300

    Fix issues in running local hack scripts
    In IPAM, there was only issues in codegen. Fix that, plus made it
    dynamically detect all the config and generated files, so they're
    included if they'd be generated elsewhere. It is in line with BMO
    generate.sh now.
    
    For others, simply adding one shellcheck ignore for a optional check
    many people have enabled about, tired of seeing my editor complain :)
    
    Signed-off-by: Tuomo Tanskanen <tuomo.tanskanen@est.tech>

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -1,39 +1,34 @@
 #!/bin/sh
 
+# Ignore the rule that says we should always quote variables, because
+# in this script we *do* want globbing.
+# shellcheck disable=SC2086,SC2292
+
 set -eux
 
 IS_CONTAINER="${IS_CONTAINER:-false}"
 ARTIFACTS="${ARTIFACTS:-/tmp}"
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-podman}"
+WORKDIR="${WORKDIR:-/workdir}"
 
 if [ "${IS_CONTAINER}" != "false" ]; then
-    export XDG_CACHE_HOME=/tmp/.cache
-    eval "$(go env)"
-    INPUT_FILES="\
-    config/certmanager/*.yaml
-    config/crd/*.yaml
-    config/crd/bases/*.yaml
-    config/crd/patches/*.yaml
-    config/default/*.yaml
-    config/manager/*.yaml
-    config/rbac/*.yaml
-    config/webhook/*.yaml
-    api/v1alpha1/zz_generated.*.go
-    ipam/mocks/zz_generated.*.go"
+    # we need to tell git its OK to use dir owned by someone else
+    git config --global safe.directory "${WORKDIR}"
+    export XDG_CACHE_HOME="/tmp/.cache"
 
-    # shellcheck disable=SC2086
+    INPUT_FILES="$(git ls-files config) $(git ls-files | grep zz_generated)"
     cksum ${INPUT_FILES} > "${ARTIFACTS}/lint.cksums.before"
     export VERBOSE="--verbose"
     make generate
-    # shellcheck disable=SC2086
     cksum ${INPUT_FILES} > "${ARTIFACTS}/lint.cksums.after"
     diff "${ARTIFACTS}/lint.cksums.before" "${ARTIFACTS}/lint.cksums.after"
+
 else
     "${CONTAINER_RUNTIME}" run --rm \
         --env IS_CONTAINER=TRUE \
-        --volume "${PWD}:/workdir:rw,z" \
+        --volume "${PWD}:${WORKDIR}:rw,z" \
         --entrypoint sh \
-        --workdir /workdir \
+        --workdir "${WORKDIR}" \
         docker.io/golang:1.22 \
-        /workdir/hack/codegen.sh
+        "${WORKDIR}"/hack/codegen.sh "$@"
 fi
