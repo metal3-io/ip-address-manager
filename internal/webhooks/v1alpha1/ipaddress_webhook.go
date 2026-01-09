a02560b325f32998d250e25b5914af4897f5cbde
Tuomo Tanskanen <tuomo.tanskanen@est.tech>
2026-01-09 10:53:19 +0200

    replace pkg/errors with stdlib Errorf
    Cleanup one direct dependency by using stdlib instead.
    
    Signed-off-by: Tuomo Tanskanen <tuomo.tanskanen@est.tech>

--- internal/webhooks/v1alpha1/ipaddress_webhook.go
+++ internal/webhooks/v1alpha1/ipaddress_webhook.go
@@ -17,10 +17,10 @@ package webhooks
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	ipamv1 "github.com/metal3-io/ip-address-manager/api/v1alpha1"
-	"github.com/pkg/errors"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
