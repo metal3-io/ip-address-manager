Nordix/metal3-ipam

Add totalIPCount and availableIPCount to IPPool status
Add totalIPCount and availableIPCount fields to IPPoolStatus so pool capacity and remaining
addresses are readable directly from the resource.

The controller computes the total as the sum of all pool sizes and the available count as the
total minus the addresses currently in use, updating them on every reconcile. Both are also
exposed as `kubectl get ippool` columns.

- Add TotalIPCount and AvailableIPCount to IPPoolStatus (with printer columns)
- Compute counts in updateAddressCounts(), wired into UpdateAddresses()
- Reuse the existing GetPoolSize() to size each pool

What this PR does / why we need it:
Previously, an IPPool's capacity and remaining addresses could only be derived by
manually summing the spec ranges and subtracting the allocations. This PR adds
totalIPCount and availableIPCount to IPPoolStatus so users can see pool usage
directly on the object, including as `kubectl get ippool` columns.

totalIPCount is the sum of GetPoolSize() across spec.pools. availableIPCount is
totalIPCount minus the number of addresses currently in use (metal3 and CAPI
allocations, including active preallocations). The counts are recalculated on
every reconcile, including during deletion as addresses are freed.

Computing them is best-effort: if a pool's size cannot be determined, reconcile
is not failed and the counts are left unset.

Fixes #161

Checklist:

- [] Documentation has been updated, if necessary.
- [] Unit tests have been added, if necessary.
- [] E2E tests have been added, if necessary.
- [] Integration tests have been added, if necessary.

Signed-off-by: Migi Jylhä <migi.jylha@est.tech>

api/v1alpha1/zz_generated.deepcopy.go
@@ -327,6 +327,16 @@ func (in *IPPoolStatus) DeepCopyInto(out *IPPoolStatus) {
 			(*out)[key] = val
 		}
 	}
+	if in.TotalIPCount != nil {
+		in, out := &in.TotalIPCount, &out.TotalIPCount
+		*out = new(int64)
+		**out = **in
+	}
+	if in.AvailableIPCount != nil {
+		in, out := &in.AvailableIPCount, &out.AvailableIPCount
+		*out = new(int64)
+		**out = **in
+	}
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new IPPoolStatus.

metal3-io/ip-address-manager

api/v1alpha1/zz_generated.deepcopy.go
@@ -327,6 +327,16 @@ func (in *IPPoolStatus) DeepCopyInto(out *IPPoolStatus) {
 			(*out)[key] = val
 		}
 	}
+	if in.Capacity != nil {
+		in, out := &in.Capacity, &out.Capacity
+		*out = new(int64)
+		**out = **in
+	}
+	if in.AvailableIPCount != nil {
+		in, out := &in.AvailableIPCount, &out.AvailableIPCount
+		*out = new(int64)
+		**out = **in
+	}
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new IPPoolStatus.